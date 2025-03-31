380fe90e40e67c0d6d533fcbc5a856fe8ddbbbe1
hana2212 <nthaiyen0606@gmail.com>
2025-03-31 10:01:28 +0700

    value help #1

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,10 +1,35 @@
+// filepath: e:\fiori_v1\webapp\controller\App.controller.js
 sap.ui.define([
-  "sap/ui/core/mvc/Controller"
-], (BaseController) => {
+  "sap/ui/core/mvc/Controller",
+  "sap/ui/core/Fragment"
+], function (Controller, Fragment) {
   "use strict";
 
-  return BaseController.extend("fiori2.controller.App", {
-      onInit() {
-      }
+  return Controller.extend("fiori2.controller.App", {
+      onInit: function () {
+          // Initialization code
+      },
+
+      // onOpenValueHelpDialog: function () {
+      //     var oView = this.getView();
+
+      //     if (!this._pValueHelpDialog) {
+      //         this._pValueHelpDialog = Fragment.load({
+      //             id: oView.getId(),
+      //             name: "fiori2.controller.ValueHelpDialogFilterbar",
+      //             controller: this
+      //         }).then(function (oDialog) {
+      //             oView.addDependent(oDialog);
+      //             return oDialog;
+      //         });
+      //     }
+      //     this._pValueHelpDialog.then(function (oDialog) {
+      //         oDialog.open();
+      //     });
+      // },
+
+      // onCloseValueHelpDialog: function () {
+      //     this.byId("valueHelpDialog").close();
+      // }
   });
 });
